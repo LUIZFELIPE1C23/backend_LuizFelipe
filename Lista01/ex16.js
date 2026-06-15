cafb9083377add38f2a279a3cc202ef0a6ace88f
Luiz <luiz.joay@escola.pr.gov.br>
2026-06-15 11:48:03 -0300

    Término da Lista 1

--- Lista01/ex16.js
+++ Lista01/ex16.js
@@ -1,17 +1,19 @@
 const turma = [
     { nome: 'Alice', nota: 10 },
-    { nome: 'Maria', nota: 6 },
+    { nome: 'Bruno', nota: 7 },
     { nome: 'Carlos', nota: 6 },
     { nome: 'Daniel', nota: 5 },
     { nome: 'Eduardo', nota: 8 }
-]
-turma.map(situacao => {
-    if(situacao.nota >= 6){
-        situacao.situacao = 'Aprovado';
-    } else {
-        situacao.situacao = 'Reprovado';
-    }   
-    return situacao;
-}
-);
-console.log(turma);
+];
+
+const turmaSituacao = turma.map(aluno => ({
+    ...aluno,
+    situacao: aluno.nota >= 7 ? 'Aprovado' : 'Reprovado'
+}));
+
+const aprovados = turmaSituacao.filter(aluno => aluno.situacao === 'Aprovado');
+const mediaGeral = turma.reduce((acum, aluno) => acum + aluno.nota, 0) / turma.length;
+
+console.log(turmaSituacao);
+console.log('Aprovados:', aprovados.length);
+console.log('Média geral:', mediaGeral);
